40f535d2c64ed54153c951ee31bab9f7d2fc9568
Tony Morris <tmorris@tmorris.net>
2015-11-30 12:40:07 +1000

    Fix getFoscamDirectoryContents to include directory

--- src/Data/Foscam/Directory.hs
+++ src/Data/Foscam/Directory.hs
@@ -3,31 +3,40 @@
 module Data.Foscam.Directory(
   getFoscamDirectoryContents
 , FoscamDirectoryFile(Isn't, Is)
+, filenames
 ) where
 
-import Data.Foscam.File.Filename(Filename, filename)
-import System.Directory(getDirectoryContents)
-import Text.Trifecta(Result(Success, Failure), parseString)
-import Text.Trifecta.Delta(Delta(Directed))
+import Control.Category(id)
 import Data.ByteString.UTF8 as UTF8(fromString)
 import Data.Eq(Eq)
-import Data.Ord(Ord)
-import System.IO(IO, FilePath)
-import Control.Category((.))
+import Data.Foscam.File.Filename(Filename, filename)
 import Data.Functor(fmap, (<$>))
+import Data.List(foldr)
+import Data.Ord(Ord)
+import System.Directory(getDirectoryContents)
+import System.IO(IO, FilePath)
+import Text.Trifecta(Result(Success, Failure), parseString)
+import Text.Trifecta.Delta(Delta(Directed))
 import Prelude(Show)
 
 data FoscamDirectoryFile =
   Isn't FilePath
-  | Is Filename
+  | Is FilePath Filename
   deriving (Eq, Ord, Show)
 
 getFoscamDirectoryContents ::
   FilePath
   -> IO [FoscamDirectoryFile]
 getFoscamDirectoryContents p =
-  let fromResult (Success n) =
-        Is n
-      fromResult (Failure _) =
-        Isn't p
-  in fmap (fromResult . parseString filename (Directed (UTF8.fromString p) 0 0 0 0)) <$> getDirectoryContents p
+  (fmap (\f -> 
+    case parseString filename (Directed (UTF8.fromString p) 0 0 0 0) f of
+      Success n -> Is p n
+      Failure _ -> Isn't f)) <$> getDirectoryContents p
+
+filenames ::
+  [FoscamDirectoryFile]
+  -> [(FilePath, Filename)]
+filenames =
+  foldr (\f -> case f of
+                 Isn't _ -> id
+                 Is p n -> ((p,n):)) []
